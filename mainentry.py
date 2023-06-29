775d03d17d3fdc1f7f5cab444b5149db75d60270
tariq <tariq.saeed81@gmail.com>
2023-06-30 01:38:56 +0300

    New code

--- mainentry.py
+++ mainentry.py
@@ -1,5 +1,5 @@
 import argparse
-
+from configparser import SafeConfigParser
 from querylineage2 import QueryLineageAnalysis
 
 if __name__ == "__main__":
@@ -12,10 +12,14 @@ if __name__ == "__main__":
     argParser.add_argument("-d", "--ddlpath", help="Path of the DDL scripts")
     argParser.add_argument("-t", "--table", help="Table name as target for the lineage")
     argParser.add_argument("-f", "--filter", help="Filter the intermediate tables [Y/N]")
-    argParser.add_argument("-b", "--databse", help="Database name for the intermediate tables to be filtered")
+    argParser.add_argument("-b", "--databse", help="Database names for the intermediate tables to be filtered(comma delimted)")
     argParser.add_argument("-g", "--graph", help="Graph type[g=Graphviz,d=DrawIO]")
-    argParser.add_argument("-s", "--styles", help="Styles file path for style.ini (table,column,edge,width,height) used by drawio")
+    argParser.add_argument("-s", "--stylespath", help="Styles file path for style.ini (table,column,edge,width,height) used by drawio")
     argParser.add_argument("-o", "--outpath", help="Output path")
+    argParser.add_argument("-c", "--space", help="Space between the source tables")
+    argParser.add_argument("-e", "--sourcetarget", help="space source to target factor")
+    argParser.add_argument("-h", "--distance", help="source to target space to be multiplied by srctgt factor")
+    argParser.add_argument("-i", "--height", help="item height")
     args = argParser.parse_args()
 
     ln = QueryLineageAnalysis(args.sqlpath, args.ddlpath, "./")
@@ -23,13 +27,22 @@ if __name__ == "__main__":
     filter = False
     if args.filter and args.filter.upper() == "Y":
         filter = True
-        ln.createfilteredRelations(args.table,args.database)
-    if args.graph
+        filtedDBs = args.database.split(",")
+        ln.createfilteredRelations(args.table,filtedDBs)
+    if args.graph:
         if args.graph.lower() == "g":
             ln.createGraphviz(args.table,"./",None,filter)
             ln.writeGraphvizToPNG("{}/{}.png".format(args.outpath,args.table))
         elif args.graph.lower() == "d":
-            pass
+            parser = SafeConfigParser() ### parse style.ini
+            parser.read("{}/{}".format(args.stylespath,"style.ini"))
+            tableStyle = parser.get("style","table_style")
+            columnStyle = parser.get("style", "column_style")
+            edgeStyle = parser.get("style", "edge_style")
+            ln.generateDrawIOXMLLayout(args.table,tableStyle,columnStyle,edgeStyle,
+                                       args.outpath,args.table+".drawio",int(args.space),
+                                       int(args.sourcetarget),int(args.distance),int(args.height),filter)
+
 
 
 
